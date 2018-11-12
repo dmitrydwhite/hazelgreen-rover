631205b3222ad0ee068e31e357b04baa043b539b
Dmitry White <dmitry@kubos.co>
2018-11-12 15:37:12 -0800

    Very simple static motor command, need to add controls and other motor, etc

---
+++ src/simple-motor-control.c
@@ -0,0 +1,38 @@
+int motorA = 2; // Digital pin for direction control
+int motorB = 3; // Digital pin for direction control
+int enableA = 5; // Analog pin for motor rate
+
+/**
+ * Set our pins to their correct mode and function:
+ */
+void setup() {
+	pinMode(motorA, OUTPUT);
+	pinMode(motorB, OUTPUT);
+	pinMode(enableA, OUTPUT);
+}
+
+/**
+ * Runs motor one direction for 1 second at 200, pauses for 1 second,
+ * runs the other direction for 1 second, pauses for 1 second.
+ */
+void loop() {
+	digitalWrite(motorA, HIGH);
+	digitalWrite(motorB, LOW);
+	analogWrite(enableA, 200);
+	delay(1000);
+
+	digitalWrite(motorA, LOW);
+	digitalWrite(motorB, LOW);
+	analogWrite(enableA, 0);
+	delay(1000);
+
+	digitalWrite(motorA, LOW);
+	digitalWrite(motorB, HIGH);
+	analogWrite(enableA, 200);
+	delay(1000);
+
+	digitalWrite(motorA, LOW);
+	digitalWrite(motorB, LOW);
+	analogWrite(enableA, 0);
+	delay(1000);
+}
